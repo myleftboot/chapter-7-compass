eaffcfd1b8987c973d7635deda71f8bbab2ff38e
Darren Cope <github@darren.cope.name>
2013-01-11 14:44:20 +0000

    Update Resources/app.js

--- Resources/app.js
+++ Resources/app.js
@@ -18,10 +18,38 @@ var direction = Ti.UI.createLabel({});
 function updateLabels(_args) {
 	compassHeading.text = _args.magneticHeading+ ' degrees';
 	
-	var heading = null;
-	
-	
-	direction.text = heading;
+	var headingText = null;
+	var h = _args.magneticHeading;
+	switch(true) {
+		case h>=0&&h<23:
+			headingText = 'N';
+			break;
+		case h>=23&&h<68:
+			headingText = 'NE';
+			break;
+		case h>=68&&h<113:
+			headingText = 'E';
+			break;
+		case h>=113&&h<158:
+			headingText = 'SE';
+			break;
+		case h>=158&&h<203:
+			headingText = 'S';
+			break;
+		case h>=203&&h<248:
+			headingText = 'SW';
+			break;
+		case h>=248&&h<293:
+			headingText = 'W';
+			break;
+		case h>=293&&h<338:
+			headingText = 'NW';
+			break;
+		case h>=338&&h<=360:
+			headingText = 'N';
+			break;
+             }
+	direction.text = 'You are looking '+headingText;
 }
 
 Ti.Geolocation.addEventListener("heading", updateLabels);
